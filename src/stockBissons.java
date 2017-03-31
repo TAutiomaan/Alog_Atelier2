4b46dce62630308056a8c0f3671bd7f34565c36c
SadmiBouhafs <sadmibouhafs@gmail.com>
2017-03-31 11:47:45 +0100

    I added stockBoissons class

---
+++ src/stockBissons.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class stockBissons {
+	
+	
+	private HashMap<String, Integer> stock = new HashMap(); 
+	
+	
+	private void distribuerBoisson(String boisson){
+		
+	}
+	
+	private void alimenterStock(String boisson){
+		
+	}
+
+}
